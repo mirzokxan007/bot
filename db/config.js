54d930b064ba83c264b92bc0775bd8b4b2ce5fe2
Mirzo <axror.usmonov5051@gmail.com>
2023-10-17 17:13:54 +0500

    server change

--- db/config.js
+++ db/config.js
@@ -3,13 +3,17 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const client = new Pool({
-  user: "postgres",
-  database: "bot",
-  port: 5432,
-  host: "localhost",
-  password: "0007",
-});
+    connectionString:
+      "postgres://bot_o128_user:jDtrMZdLHHDNiq1FgBrmhJehyYTfVW5D@dpg-ckjoaocl4vmc73a627v0-a.oregon-postgres.render.com/bot_o128",
+    ssl: {
+      rejectUnauthorized: false,
+      ca: "",
+    },
+  });
+  
+  client.on('error', (err) => {
+    console.log(err.message)
+ });
 
-client.connect();
-
-export default client;
+  
+  export default client;
